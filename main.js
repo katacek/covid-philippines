96ea807bfdf3b6aad35f2fba2a87eec0f17db60c
katacek <56041262+katacek@users.noreply.github.com>
2020-04-02 10:54:12 +0200

    test

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ let check = false;
 Apify.main(async () =>
 {
 
-    const kvStore = await Apify.openKeyValueStore('COVID-19-PH');
-    const dataset = await Apify.openDataset('COVID-19-PH-HISTORY');
-    const { email } = await Apify.getValue('INPUT');
+    //const kvStore = await Apify.openKeyValueStore('COVID-19-PH');
+    //const dataset = await Apify.openDataset('COVID-19-PH-HISTORY');
+    //const { email } = await Apify.getValue('INPUT');
 
     console.log('Launching Puppeteer...');
     const browser = await Apify.launchPuppeteer();
