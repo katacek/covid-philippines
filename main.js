a0c3d45ed8d1a8ee9b3a273a8d5db477d94d6678
katacek <56041262+katacek@users.noreply.github.com>
2020-04-02 10:52:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ let check = false;
 Apify.main(async () =>
 {
 
-    // const kvStore = await Apify.openKeyValueStore('COVID-19-UK');
-    // const dataset = await Apify.openDataset('COVID-19-UK-HISTORY');
-    // const { email } = await Apify.getValue('INPUT');
+    const kvStore = await Apify.openKeyValueStore('COVID-19-PH');
+    const dataset = await Apify.openDataset('COVID-19-PH-HISTORY');
+    const { email } = await Apify.getValue('INPUT');
 
     console.log('Launching Puppeteer...');
     const browser = await Apify.launchPuppeteer();
@@ -20,17 +20,6 @@ Apify.main(async () =>
     console.log('Going to the website...');
     await page.goto('https://ncovtracker.doh.gov.ph/'), { waitUntil: 'networkidle0', timeout: 60000 };
     
-    // const trackCoronavirusCases = '#attachment_4091163 > div.attachment-details > h2 > a';
-    // //const trackCoronavirusCases = '#attachment_4077017 > div.attachment-details > h2 > a';
-    // await page.waitForSelector(trackCoronavirusCases);
-
-    // const trackCoronavirusCasesLink = await page.$eval(trackCoronavirusCases, el => el.href);
-    // // networkidle0 : wait for all page to load
-    // await page.goto(trackCoronavirusCasesLink), { waitUntil: 'networkidle0', timeout: 60000 };
-   
-    // await page.waitForSelector('#UK_Countries_cases_976_layer');
-    // await page.waitForSelector('div.flex-fluid');
-    //await page.waitFor(4000);
     await page.waitForSelector("text[vector-effect='non-scaling-stroke']");
  
     console.log('Getting data...');
@@ -38,10 +27,6 @@ Apify.main(async () =>
     const result = await page.evaluate(() =>
     {
 
-        // const getInt = (x)=>{
-        //     return parseInt(x.replace(' ','').replace(',',''))};
-        // const now = new Date();
-        
         // eq() selector selects an element with a specific index number, text() method sets or returns the text content of the selected elements
         const confirmed = $("text[vector-effect='non-scaling-stroke']").eq(1).text();
         const PUIs = $("text[vector-effect='non-scaling-stroke']").eq(3).text();
@@ -59,9 +44,9 @@ Apify.main(async () =>
             PUMs: PUMs,
             country: "Phillipines",
             //historyData: "https://api.apify.com/v2/datasets/K1mXdufnpvr53AFk6/items?format=json&clean=1",
-            // sourceUrl:'https://www.gov.uk/government/publications/covid-19-track-coronavirus-cases',
-            // lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
-            // lastUpdatedAtSource: "N/A",
+            sourceUrl:'https://ncovtracker.doh.gov.ph/',
+            lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
+            lastUpdatedAtSource: "N/A",
             // readMe: 'https://apify.com/katerinahronik/covid-uk',
             };
         return data;
