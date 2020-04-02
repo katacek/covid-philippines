a66ed52623d41eb73d206426a2a8460780de17e2
katacek <56041262+katacek@users.noreply.github.com>
2020-04-02 12:51:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ let check = false;
 Apify.main(async () =>
 {
 
-    //const kvStore = await Apify.openKeyValueStore('COVID-19-PH');
-    //const dataset = await Apify.openDataset('COVID-19-PH-HISTORY');
-    //const { email } = await Apify.getValue('INPUT');
+    const kvStore = await Apify.openKeyValueStore('COVID-19-PH');
+    const dataset = await Apify.openDataset('COVID-19-PH-HISTORY');
+    const { email } = await Apify.getValue('INPUT');
 
     console.log('Launching Puppeteer...');
     const browser = await Apify.launchPuppeteer();
@@ -17,13 +17,14 @@ Apify.main(async () =>
     const page = await browser.newPage();
    
     console.log('Going to the website...');
+    
+    // the source url (html page source) is just a link to this page
     await page.goto('https://dohph.maps.arcgis.com/apps/opsdashboard/index.html#/3dda5e52a7244f12a4fb3d697e32fd39'), { waitUntil: 'networkidle0', timeout: 60000 };
     await Apify.utils.puppeteer.injectJQuery(page);
     
     await page.waitForSelector("text[vector-effect='non-scaling-stroke']");
     await page.waitFor(4000);
-    //await page.waitForSelector(".esriMapContainer");
- 
+    
     console.log('Getting data...');
     // page.evaluate(pageFunction[, ...args]), pageFunction <function|string> Function to be evaluated in the page context, returns: <Promise<Serializable>> Promise which resolves to the return value of pageFunction
     const result = await page.evaluate(() =>
@@ -62,26 +63,26 @@ Apify.main(async () =>
     
     console.log(result)
     
-    // if ( !result.infected || !result.dailyConfirmed || !result.deceased|| !result.englandConfirmed|| !result.scottlandConfirmed|| !result.walesConfirmed|| !result.northenIrelandConfirmed) {
-    //             check = true;
-    //         }
-    // else {
-    //         let latest = await kvStore.getValue(LATEST);
-    //         if (!latest) {
-    //             await kvStore.setValue('LATEST', result);
-    //             latest = result;
-    //         }
-    //         delete latest.lastUpdatedAtApify;
-    //         const actual = Object.assign({}, result);
-    //         delete actual.lastUpdatedAtApify;
+    if ( !result.infected || !result.tested || !result.deceased|| !result.recovered|| !result.deceased|| !result.PUIs|| !result.PUMs) {
+                check = true;
+            }
+    else {
+            let latest = await kvStore.getValue(LATEST);
+            if (!latest) {
+                await kvStore.setValue('LATEST', result);
+                latest = result;
+            }
+            delete latest.lastUpdatedAtApify;
+            const actual = Object.assign({}, result);
+            delete actual.lastUpdatedAtApify;
 
-    //         if (JSON.stringify(latest) !== JSON.stringify(actual)) {
-    //             await dataset.pushData(result);
-    //         }
+            if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+                await dataset.pushData(result);
+            }
 
-    //         await kvStore.setValue('LATEST', result);
-    //         await Apify.pushData(result);
-    //     }
+           await kvStore.setValue('LATEST', result);
+           await Apify.pushData(result);
+         }
 
 
     console.log('Closing Puppeteer...');
@@ -89,18 +90,18 @@ Apify.main(async () =>
     console.log('Done.');  
     
     // if there are no data for TotalInfected, send email, because that means something is wrong
-    // const env = await Apify.getEnv();
-    // if (check) {
-    //     await Apify.call(
-    //         'apify/send-mail',
-    //         {
-    //             to: email,
-    //             subject: `Covid-19 UK from ${env.startedAt} failed `,
-    //             html: `Hi, ${'<br/>'}
-    //                     <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
-    //                     run had 0 TotalInfected, check it out.`,
-    //         },
-    //         { waitSecs: 0 },
-    //     );
-    // };
+    const env = await Apify.getEnv();
+      if (check) {
+        await Apify.call(
+            'apify/send-mail',
+            {
+                to: email,
+                subject: `Covid-19 Philippines from ${env.startedAt} failed `,
+                html: `Hi, ${'<br/>'}
+                        <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
+                        run had 0 in one of the variables, check it out.`,
+            },
+            { waitSecs: 0 },
+        );
+    };
 });
