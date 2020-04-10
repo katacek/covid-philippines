360c6d79948739c2aaa7a72e8f25088e67c4c2ad
katacek <katacek@gmail.com>
2020-04-10 19:03:07 +0200

    try catch

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ Apify.main(async () =>
     const dataset = await Apify.openDataset('COVID-19-PH-HISTORY');
     const { email } = await Apify.getValue('INPUT');
 
+    try{
+
+
     console.log('Launching Puppeteer...');
     const browser = await Apify.launchPuppeteer();
 
@@ -93,18 +96,33 @@ Apify.main(async () =>
     console.log('Done.');  
     
     // if there are no data for TotalInfected, send email, because that means something is wrong
-    const env = await Apify.getEnv();
-      if (check) {
-        await Apify.call(
-            'apify/send-mail',
-            {
-                to: email,
-                subject: `Covid-19 Philippines from ${env.startedAt} failed `,
-                html: `Hi, ${'<br/>'}
-                        <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
-                        run had 0 in one of the variables, check it out.`,
-            },
-            { waitSecs: 0 },
-        );
-    };
+    // const env = await Apify.getEnv();
+    //   if (check) {
+    //     await Apify.call(
+    //         'apify/send-mail',
+    //         {
+    //             to: email,
+    //             subject: `Covid-19 Philippines from ${env.startedAt} failed `,
+    //             html: `Hi, ${'<br/>'}
+    //                     <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
+    //                     run had 0 in one of the variables, check it out.`,
+    //         },
+    //         { waitSecs: 0 },
+    //     );
+    // };
+}
+catch(err){
+
+    console.log(err)
+
+    let latest = await kvStore.getValue(LATEST);
+    var latestKvs = latest.lastUpdatedAtApify;
+    var latestKvsDate = new Date(latestKvs)
+    var d = new Date();
+    // adding two hours to d
+    d.setHours(d.getHours() - 2);
+    if (latestKvsDate < d) {
+        throw (err)
+    }
+}
 });
