0396d6c0b693b9cee2a937e7f6b1a5084765e42d
katacek <56041262+katacek@users.noreply.github.com>
2020-04-17 18:21:24 +0200

    manually getting data for 17/4

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ Apify.main(async () =>
         const sheetsInput = {
         mode: 'read',
         publicSpreadsheet: true,
-        spreadsheetId: '1eWTxpLV1xe6lfgiXmtZBRHY7pjWz-VOVLQbqRaBbzBA', // update to your ID
-        range:"04 Case Information!H:H"
+        spreadsheetId: '1wiO8371BXPulxy6ETxN7LOECSH09vUOCoeaavcBhQyM', // update to your ID
+        range:"DOH COVID Data Drop_ 20200417 - 05 Case Information!H:H"
         };
         const myData = (await Apify.call('lukaskrivka/google-sheets', sheetsInput)).output.body;
         const confirmed = myData.length;
@@ -38,7 +38,7 @@ Apify.main(async () =>
             historyData: "https://api.apify.com/v2/datasets/sFSef5gfYg3soj8mb/items?format=json&clean=1",
             sourceUrl:'https://ncovtracker.doh.gov.ph/',
             lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
-            lastUpdatedAtSource: "2020-04-16",
+            lastUpdatedAtSource: "2020-04-17",
             readMe: 'https://apify.com/katerinahronik/covid-philippines',
             };
         
